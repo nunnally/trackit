1d1f5ba32f1b242b7a1ec64a3091d8b87b9ff112
César Augusto <cesarisadog@gmail.com>
2021-05-20 04:49:16 -0300

    Generic Components (?)
    Trying to do generic components to be used in future projects

---
+++ src/components/UI/Row.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+const Row = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex: 0 1 auto;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-right: ${p =>p}rem;
+  margin-left: ${p => p}rem;
+  ${p => p.reverse && `
+    flex-direction: row-reverse;
+  `}
+  ${p => p.start`
+    justify-content: flex-start;
+  `}
+  ${p => p.center`
+    justify-content: center;
+  `}
+  ${p => p.end`
+    justify-content: flex-end;
+  `}
+  ${p => p.top`
+    align-items: flex-start;
+  `}
+  ${p => p.middle`
+    align-items: center;
+  `}
+  ${p => p.bottom`
+    align-items: flex-end;
+  `}
+  ${p => p.around`
+    justify-content: space-around;
+  `}
+  ${p => p.between`
+    justify-content: space-between;
+  `}
+  ${p => p.first`
+    order: -1;
+  `}
+  ${p => p.last`
+    order: 1;
+  `}
+`
+
+export default Row
